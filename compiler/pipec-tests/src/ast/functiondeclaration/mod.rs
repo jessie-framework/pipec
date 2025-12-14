846f365467158eb4565db1719e2497d7f900348e
EvoPot <evolvedpot@gmail.com>
2025-12-14 16:37:57 +0300

    pipec-ast : introduce function declarations

---
+++ compiler/pipec-tests/src/ast/functiondeclaration/mod.rs
@@ -0,0 +1,6 @@
+use crate::test_file_generation;
+
+#[test]
+fn test_function_declaration() {
+    test_file_generation!("test.pipec");
+}
--- compiler/pipec-tests/src/ast/mod.rs
+++ compiler/pipec-tests/src/ast/mod.rs
@@ -1,2 +1,3 @@
+mod functiondeclaration;
 mod variablemutability;
 mod viewportdeclaration;
